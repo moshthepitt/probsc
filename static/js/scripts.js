8d339dd73e8c7f212376ae747c58407193b44e70
moshthepitt <kelvin@jayanoris.com>
2017-04-12 09:44:57 +0300

    auto hide alerts

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,5 @@
 window.setTimeout(function() {
-    $(".alert-dismissable").fadeTo(500, 0).slideUp(500, function(){
+    $(".alert-dismissible").fadeTo(500, 0).slideUp(500, function(){
         $(this).remove(); 
     });
-}, 3000);
+}, 4000);
